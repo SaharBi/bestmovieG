e348703774246621730ceed40bf41484d9912d51
Sahar Bitton <saharbi@walla.co.il>
2020-08-08 11:04:49 +0300

    Updates

---
+++ precache-manifest.b9cfe4aabf92cf1339c639232140297f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445684e98fef994972d129bce48a47f5",
+    "url": "/bestmovieG/index.html"
+  },
+  {
+    "revision": "a78b8d6f98a1c6d0efba",
+    "url": "/bestmovieG/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f773f7136e9210faa5b9",
+    "url": "/bestmovieG/static/css/main.43dcfbbb.chunk.css"
+  },
+  {
+    "revision": "a78b8d6f98a1c6d0efba",
+    "url": "/bestmovieG/static/js/2.64b245c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bestmovieG/static/js/2.64b245c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f773f7136e9210faa5b9",
+    "url": "/bestmovieG/static/js/main.5239c224.chunk.js"
+  },
+  {
+    "revision": "55e24e4c2632ff3811f5",
+    "url": "/bestmovieG/static/js/runtime-main.d69cc204.js"
+  },
+  {
+    "revision": "78a5427f3d828c1897939f84971b3f2b",
+    "url": "/bestmovieG/static/media/img.78a5427f.jpg"
+  },
+  {
+    "revision": "749a10bf9dbe483ed482ecc2ecc1573a",
+    "url": "/bestmovieG/static/media/img4.749a10bf.jpg"
+  }
+]);
